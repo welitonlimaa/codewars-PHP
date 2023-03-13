e5b8c56019d92b99f6b5601c69280c6f4c288975
welitonlimaa <welitonlima159@gmail.com>
2023-03-13 11:13:18 -0300

    Product Of Consecutive Fib Numbers Challenge

---
+++ 7kyu/productOfConsecutiveFibNumbers.php
@@ -0,0 +1,36 @@
+<?php
+// The Fibonacci numbers are the numbers in the following integer sequence (Fn):
+
+// 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, ...
+
+// such as
+
+// F(n) = F(n-1) + F(n-2) with F(0) = 0 and F(1) = 1.
+
+// Given a number, say prod (for product), we search two Fibonacci numbers F(n) and F(n+1) verifying
+
+// F(n) * F(n+1) = prod.
+
+// Your function productFib takes an integer (prod) and returns an array:
+
+// [F(n), F(n+1), true] or {F(n), F(n+1), 1} or (F(n), F(n+1), True)
+// depending on the language if F(n) * F(n+1) = prod.
+
+// If you don't find two consecutive F(n) verifying F(n) * F(n+1) = prodyou will return
+
+// [F(n), F(n+1), false] or {F(n), F(n+1), 0} or (F(n), F(n+1), False)
+// F(n) being the smallest one such as F(n) * F(n+1) > prod.
+
+function productFib($prod) {
+	$n1 = 0;  
+	$n2 = 1;    
+	while (($n1 * $n2) < $prod ) {
+	$n3 = $n2 + $n1;   
+	$n1 = $n2;  
+	$n2 = $n3;     
+	}
+	$status = ($n1 * $n2) === $prod;
+	
+	return [$n1, $n2, $status];
+}
+?>
